6bb360de17045e5d094f525ee9ffd20e27360ef3
kaiser2012 <kaiser_chen@feiliks.com>
2018-02-24 17:15:19 +0800

    滑动删除特效测试

---
+++ js/touchWipe.js
@@ -0,0 +1,120 @@
+﻿(function ($) {
+    $.fn.touchWipe = function (option) {
+        var defaults = {
+            itemDelete: '.item-delete', //删除元素
+        };
+        var opts = $.extend({}, defaults, option); //配置选项
+        var delWidth = $(opts.itemDelete).width();
+
+        var initX; //触摸位置X
+        var initY; //触摸位置Y
+        var moveX; //滑动时的位置X
+        var moveY; //滑动时的位置Y
+        var X = 0; //移动距离X
+        var Y = 0; //移动距离Y
+        var flagX = 0; //是否是左右滑动 0为初始，1为左右，2为上下，在move中设置，在end中归零
+        var objX = 0; //目标对象位置
+
+        $(this).on('touchstart', function (event) {
+            console.log('start..');
+            var obj = this;
+            initX = event.targetTouches[0].pageX;
+            initY = event.targetTouches[0].pageY;
+            console.log(initX + ':' + initY);
+            objX = (obj.style.WebkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+            console.log(objX);
+            if (objX == 0) {
+                $(this).on('touchmove', function (event) {
+
+                    // 判断滑动方向，X轴阻止默认事件，Y轴跳出使用浏览器默认
+                    if (flagX == 0) {
+                        setScrollX(event);
+                        return;
+                    } else if (flagX == 1) {
+                        event.preventDefault();
+                    } else {
+                        return;
+                    }
+
+                    var obj = this;
+                    moveX = event.targetTouches[0].pageX;
+                    X = moveX - initX;
+                    if (X >= 0) {
+                        obj.style.WebkitTransform = "translateX(" + 0 + "px)";
+                    } else if (X < 0) {
+                        var l = Math.abs(X);
+                        obj.style.WebkitTransform = "translateX(" + -l + "px)";
+                        if (l > delWidth) {
+                            l = delWidth;
+                            obj.style.WebkitTransform = "translateX(" + -l + "px)";
+                        }
+                    }
+                });
+            } else if (objX < 0) {
+                $(this).on('touchmove', function (event) {
+
+                    // 判断滑动方向，X轴阻止默认事件，Y轴跳出使用浏览器默认
+                    if (flagX == 0) {
+                        setScrollX(event);
+                        return;
+                    } else if (flagX == 1) {
+                        event.preventDefault();
+                    } else {
+                        return;
+                    }
+
+                    var obj = this;
+                    moveX = event.targetTouches[0].pageX;
+                    X = moveX - initX;
+                    if (X >= 0) {
+                        var r = -delWidth + Math.abs(X);
+                        obj.style.WebkitTransform = "translateX(" + r + "px)";
+                        if (r > 0) {
+                            r = 0;
+                            obj.style.WebkitTransform = "translateX(" + r + "px)";
+                        }
+                    } else { //向左滑动
+                        obj.style.WebkitTransform = "translateX(" + -delWidth + "px)";
+                    }
+                });
+            }
+        })
+
+        //结束时判断，并自动滑动到底或返回
+        $(this).on('touchend', function (event) {
+            var obj = this;
+            objX = (obj.style.WebkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+            if (objX > -delWidth / 2) {
+                obj.style.transition = "all 0.2s";
+                obj.style.WebkitTransform = "translateX(" + 0 + "px)";
+                obj.style.transition = "all 0";
+                objX = 0;
+            } else {
+                obj.style.transition = "all 0.2s";
+                obj.style.WebkitTransform = "translateX(" + -delWidth + "px)";
+                obj.style.transition = "all 0";
+                objX = -delWidth;
+            }
+            flagX = 0;
+        })
+
+        //设置滑动方向
+        function setScrollX(event) {
+            moveX = event.targetTouches[0].pageX;
+            moveY = event.targetTouches[0].pageY;
+            X = moveX - initX;
+            Y = moveY - initY;
+
+            if (Math.abs(X) > Math.abs(Y)) {
+                flagX = 1;
+            } else {
+                flagX = 2;
+            }
+            return flagX;
+        }
+
+        //链式返回
+        return this;
+    };
+
+})(Zepto);
---
+++ test2.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test2.aspx.cs" Inherits="WeChat.test2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>登录界面</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+    <script type='text/javascript' src='//g.alicdn.com/sj/lib/zepto/zepto.min.js' charset='utf-8'></script>
+    <link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css" />
+
+    <style>
+    *{ padding:0; margin:0; list-style: none;}
+    header{ background: #f7483b; border-bottom: 1px solid #ccc}
+    header h2{ text-align: center; line-height: 54px; font-size: 16px; color: #fff}
+   .list-ul{ overflow: hidden}
+    .list-li{ line-height: 60px; border-bottom: 1px solid #fcfcfc; position:relative;padding: 0 12px; color: #666;
+       background: #f2f2f2;
+       -webkit-transform: translateX(0px);
+   }
+    .btn{ position: absolute; top: 0; right: -80px; text-align: center; background: #999; color: #fff; width: 80px}
+</style>
+</head>
+<body>
+<header>
+    <h2>消息列表</h2>
+  </header>
+  <section class="list">
+    <ul class="list-ul">
+      <li id="li" class="list-li">
+        <div class="con">
+          <div>你的快递到了，请到楼下签收</div>
+          <div>你的快递到了，请到楼下签收</div>
+          <div>你的快递到了，请到楼下签收</div>
+        </div>
+        <div class="btn" onclick="show()">删除</div>
+      </li>
+      <li class="list-li">
+        <div class="con">
+          <div>哇，你在干嘛，快点来啊就等你了</div>
+          <div>哇，你在干嘛，快点来啊就等你了</div>
+          <div>哇，你在干嘛，快点来啊就等你了</div>
+          <div>哇，你在干嘛，快点来啊就等你了</div>
+        </div>
+        <div class="btn">删除</div>
+      </li>
+    </ul>
+  </section>
+
+ 
+<%--<script type="text/javascript" src="http://apps.bdimg.com/libs/zepto/1.1.4/zepto.min.js"></script>--%>
+<script type="text/javascript" src="js/touchWipe.js"></script>
+<script type="text/javascript">
+    $(function () {
+        $('.list-li').touchWipe({ itemDelete: '.btn' });
+    });
+    function show()
+    {
+        alert("删除成功");
+    }
+</script>
+</body>
+</html>
